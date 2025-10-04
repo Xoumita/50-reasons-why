3e5ccb1a5a846effbf9248afd355b5c33e866133
Xoumita <soumita2003d1@gmail.com>
2025-10-05 03:32:17 +0530

    update

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ function updatePage() {
           <br><br>
           🎉 Happy Birthday, my love 💕
           <br><br>
-          <button onclick="showSecretPage()" style="margin-top:15px;padding:10px 20px;border:none;background:#d63384;color:#fff;border-radius:8px;cursor:pointer;">
+          <button onclick="showSecretPage()" style="margin-top:15px;padding:10px 20px;border:none;background:#d63384;color:#fff;border-radius:8px;cursor:pointer;font-family: 'Pacifico', cursive;">
           One More Reason?
           </button>
       </div>
